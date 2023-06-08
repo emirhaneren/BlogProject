99902dbd9432149feb4c35a4a837679512ea1a51
emirhaneren <emirhanneren52@gmail.com>
2023-06-08 10:13:56 +0300

    08.06.2023

---
+++ DataAccesLayer/Migrations/MigBlogRating.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmigrationa1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogsRate",
+                columns: table => new
+                {
+                    BlogRateID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogID = table.Column<int>(type: "int", nullable: false),
+                    BlogTotalScore = table.Column<int>(type: "int", nullable: false),
+                    BlogRateCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogsRate", x => x.BlogRateID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogsRate");
+        }
+    }
+}
