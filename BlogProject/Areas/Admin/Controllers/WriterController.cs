b940ad5de4ae6227b72d3a26fda51561181c0ec0
emirhaneren <emirhanneren52@gmail.com>
2023-06-14 15:53:49 +0300

    14.06.2023

---
+++ BlogProject/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,44 @@
+﻿using BlogProject.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(Writers);
+            return Json(jsonWriters);
+        }
+        public static List<WriterClass> Writers = new List<WriterClass>()
+        {
+            new WriterClass()
+            {
+                Id = 1,
+                Name="Şenol"
+            },
+            new WriterClass()
+            {
+                Id=2,
+                Name="Sergen"
+            },
+            new WriterClass()
+            {
+                Id=3,
+                Name="Atiba"
+
+            },
+            new WriterClass()
+            {
+                Id=4,
+                Name="Baba Hakkı"
+            }
+        };
+    }
+}
--- BlogProject/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ BlogProject/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,8 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Editing;
+using System.Xml.Linq;
 
 namespace BlogProject.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +16,15 @@ namespace BlogProject.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2=c.Contacts.Count();
             ViewBag.v3=c.Comments.Count();
+            string ApiKey = "eae424ef70dbab7f5a9c925f2c1a10a3";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&units=metric&lang=tr&appid=" + ApiKey;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4=document.Descendants("temperature")
+                .ElementAt(0)
+                .Attribute("value").Value;
+            ViewBag.v5 = document.Descendants("weather")
+                .ElementAt(0)
+                .Attribute("value").Value;
             return View();
         }
     }
