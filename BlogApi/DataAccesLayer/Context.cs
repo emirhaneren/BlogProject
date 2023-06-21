0ea3da18798ca4dc2e1896ad134f45543662d54d
emirhaneren <emirhanneren52@gmail.com>
2023-06-21 16:24:31 +0300

    21.06.2023

---
+++ BlogApi/DataAccesLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.DataAccesLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //server location; DB name; security
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=DbCoreBlogApi;integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ BlogProject/Controllers/ApiTestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using Newtonsoft.Json;
+
+namespace BlogProject.Controllers
+{
+    public class ApiTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient= new HttpClient();
+            var responseMesssage = await httpClient.GetAsync("https://localhost:7001/api/Default");
+            var jsonString=await responseMesssage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<tempEmployeeClass>>(jsonString);
+            return View(values);
+        }
+    }
+    public class tempEmployeeClass
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+    }
+}
